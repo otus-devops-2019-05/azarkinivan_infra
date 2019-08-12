262c9f07df61e4621c19701d678a1b65f56c5d0e
Ivan Azarkin <azarkinivan@gmail.com>
2019-08-12 23:35:15 +0300

    add new conf files

---
+++ terraform/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  # Версия terraform
+  required_version = "~> 0.11.11"
+}
+
+provider "google" {
+  # Версия провайдера
+  version = "2.0.0"
+
+  # ID проекта
+  project = "infra-244708"
+  region  = "europe-west1"
+}
+
+resource "google_compute_instance" "app" {
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "europe-west1-b"
+  tags = ["reddit-app"]
+ 
+ # определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = "reddit-base-1563911713"
+    }
+}
+
+  metadata {
+    # путь до публичного ключа
+    ssh-keys = "appuser:${file("~/.ssh/appuser.pub")}"
+}
+
+  # определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {}
+  }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+
+  # Название сети, в которой действует правило
+  network = "default"
+
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-app"]
+}	
